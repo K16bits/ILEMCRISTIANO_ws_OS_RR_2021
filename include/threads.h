d4945704e41d496e72bbb77912d0ff12475e9352
Ilem Santos <ilemlima@gmail.com>
2021-09-06 20:52:10 -0400

    inclusão da biblioteca semaforo

--- include/threads.h
+++ include/threads.h
@@ -2,7 +2,10 @@
 #define THREADS_H_INCLUDED
 
 #include <pthread.h>
+#include <semaphore.h>
 #include "buffer.h"
+
+pthread_t pid_1, pid_2, pid_3, pid_4;
 
 void my_thread_1(file_t *db);
 void my_thread_2(file_t *db);
